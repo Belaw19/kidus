78fc3c60cb884d5ea8bca58ea62645cb178959b4
kidus girma <kidusgirma96@gmail.com>
2026-04-03 10:29:10 -0700

    Merge remote changes

--- go_routing_demo/lib/main.dart
+++ go_routing_demo/lib/main.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/material.dart';
-import 'routes.dart';
+import 'package:go_router/go_router.dart';
 
-void main() {
-  runApp(MyApp());
+class homescreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => context.go('/details'),
+          child: const Text('Go to Details'),
+        ),
+      ),
+    );
+  }
+}
+
+class detailsscreen extends StatelessWidget {
+  const detailsscreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Details')),
+      body: const Center(child: Text('This is the details screen')),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: 'go_router lesson 1',
+      routerConfig: _router,
       debugShowCheckedModeBanner: false,
-      routerConfig: router,
     );
   }
 }
+void main() {
+  runApp(const MyApp());
+}
+final _router = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => homescreen()),
+    GoRoute(path: '/details', builder: (context, state) => detailsscreen()),
+  ],
+);
