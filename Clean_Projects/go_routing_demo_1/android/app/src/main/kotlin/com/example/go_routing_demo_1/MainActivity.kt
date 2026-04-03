5ea6eaa06b2abb52b65a26121fe94df53644d5d3
kidus girma <kidusgirma96@gmail.com>
2026-04-03 13:59:03 -0700

    Remove all projects except Clean_Projects

--- Clean_Projects/go_routing_demo_1/android/app/src/main/kotlin/com/example/go_routing_demo_1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.go_routing_demo_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Clean_Projects/go_routing_demo_2/android/app/src/main/kotlin/com/example/go_routing_demo_2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.go_routing_demo_2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
